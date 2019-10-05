0a9abb20781d494a2e97bad5a85bba6aaaf0122b
Sebastian Ortiz Velez <sebasortiz2000@gmail.com>
2019-10-05 12:09:57 +0200

    Upload initial project

---
+++ Postcards/Postcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Postcards
+//
+//  Created by Sebastian Ortiz Velez on 05/10/2019.
+//  Copyright © 2019 Sebastian Ortiz Velez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
