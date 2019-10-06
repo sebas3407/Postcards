bb784d718ed65a45dbf1c85f71d6ea52b9387d18
Sebastian Ortiz Velez <sebasortiz2000@gmail.com>
2019-10-06 14:10:03 +0200

    Implement collection view

---
+++ Postcards/Postcards/Controllers/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Postcards
+//
+//  Created by Sebastian Ortiz Velez on 05/10/2019.
+//  Copyright © 2019 Sebastian Ortiz Velez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var postcardImageView: UIImageView!
+    @IBOutlet weak var colorCollectionView: UICollectionView!
+    
+    var colors = [UIColor]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.colors += [.black, .gray, .white, .red, .orange, .yellow, .green, .cyan, .blue, .purple, .magenta, .brown]
+        
+        for hue in 0...9{
+            for sat in 1...10{
+                let color = UIColor(hue: CGFloat(hue/10), saturation: CGFloat(sat/10), brightness: 1.0, alpha: 1.0)
+                colors.append(color)
+            }
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ColorCell", for: indexPath)
+        
+        let color = colors[indexPath.row]
+        cell.backgroundColor = color
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 5
+        return cell
+    }
+}
